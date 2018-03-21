1a67d1f5f9a6213d16451b95982e2238a7ce8456
Eva Antipina and Jimmy McNamara <jimmy@>
2018-03-21 13:26:53 -0700

    add js feature to allow user to add, subtract, multiply, or divide.

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ var multiply = function(number1,number2){
 }
 //alert(multiply(number1, number2));
 
-var substract = function(number1, number2){
+var subtract = function(number1, number2){
   return number1 - number2;
 
 }
-//alert(substract(number1, number2));
+//alert(subtract(number1, number2));
 
 var divide = function(number1, number2){
   return number1 / number2;
@@ -26,12 +26,40 @@ var divide = function(number1, number2){
 // Above is business end logic, below is user interface logic
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("button#addButton").click(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
     var result = add(number1, number2);
     $("#output").text(result);
+
+  });
+
+  $("button#subtractButton").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+
+  });
+
+  $("button#multiplyButton").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+
+  });
+
+  $("button#divideButton").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+
   });
 
 });
