02cbecf61c52ec0e87d717018b70aa8fe6fff431
Eva Antipina and Jimmy McNamara <eva@>
2018-03-21 11:19:02 -0700

    add forms in html and jQuery functions and variables to add user inputted numbers

--- js/script.js
+++ js/script.js
@@ -2,23 +2,36 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+//var number1 = parseInt(prompt("Enter a number:"));
+//var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
+//alert(add(number1, number2));
 
 var multiply = function(number1,number2){
   return number1 * number2;
 }
-alert(multiply(number1, number2));
+//alert(multiply(number1, number2));
 
 var substract = function(number1, number2){
   return number1 - number2;
 
 }
-alert(substract(number1, number2));
+//alert(substract(number1, number2));
 
 var divide = function(number1, number2){
   return number1 / number2;
 }
-alert(divide(number1, number2));
+//alert(divide(number1, number2));
+
+// Above is business end logic, below is user interface logic
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+});
